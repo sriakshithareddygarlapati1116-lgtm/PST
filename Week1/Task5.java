7de951c632a2acca29f9981b38df487369177408
Garlapati Sri Akshitha <sriakshithareddygarlapati1116@gmail.com>
2026-01-30 12:05:16 +0530

    Create Task5.java

---
+++ Week1/Task5.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class KthSmallest {
+    public static void main(String[] args) {
+        int[] arr = {7, 10, 4, 3, 20, 15};
+        int k = 3;
+
+        Arrays.sort(arr);
+
+        if (k > 0 && k <= arr.length) {
+            System.out.println("Kth smallest element: " + arr[k - 1]);
+        } else {
+            System.out.println("Invalid value of K");
+        }
+    }
+}
